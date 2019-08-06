d0971ef53de32719f38ca08dd28440bb882ec5d7
chunkschen <suzaku@suzaku.me>
2019-08-06 17:45:33 +0800

    增加注释

--- tools/core/base_excel.py
+++ tools/core/base_excel.py
@@ -9,12 +9,22 @@ class BaseExcel(object):
     具体逻辑处理类可继承该类编写相应逻辑
     """
     def __init__(self, path):
+        """
+        :param path: excel 文件路径
+        """
         self.path = path
         self.excel = load_workbook(path, data_only=True)
 
     def parse_sheet(self, sheet_class=None):
         """
         解析 excel 表中的 sheet，并 yield 返回 BaseSheet 实例
+        通过循环可得到excel的每个表的 BaseSheet 实例
+
+        eg:
+        obj = BaseExcel(excel_file_path)
+        for sheet_parser in obj.parse_sheet():
+            print sheet_parser.grid_list  # 获取所有数据单元格
+
         :param sheet_class: 可动态传入 sheet 处理类，否则使用 BaseSheet
         :return:
         """
